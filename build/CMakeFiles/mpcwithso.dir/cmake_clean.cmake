03b0b9fa72e42340821ec2a5b7f318b2b69b0c3b
root <409197863@qq.com>
2024-02-04 02:51:22 +0000

    Add all files to the repository

---
+++ build/CMakeFiles/mpcwithso.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mpcwithso.dir/src/main.c.o"
+  "CMakeFiles/mpcwithso.dir/src/main.c.o.d"
+  "mpcwithso"
+  "mpcwithso.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/mpcwithso.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/mpcwithso.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for mpcwithso.
+# This may be replaced when dependencies are built.
